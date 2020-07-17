229827b490ea1cdd4ee2eef435dede36e6af2e32
Manal <em_slamani@esi.dz>
2020-07-17 02:22:47 +0100

    Added the HTML pages

--- Accidents/settings.py
+++ Accidents/settings.py
@@ -130,5 +130,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATICFILES_LOCATION='static'
 STATIC_URL = '/static/'
+STATIC_ROOT='static'
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR,'Accidents/static')
+]
