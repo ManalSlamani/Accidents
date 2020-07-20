a0cae4692e3fe40365f6b921044343499f5c38d2
soumia_saadoun <es_saadoun@esi.dz>
2020-07-20 20:19:39 +0100

    bdd

--- Accidents/settings.py
+++ Accidents/settings.py
@@ -74,11 +74,14 @@ WSGI_APPLICATION = 'Accidents.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-"""
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'accidentdb',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'root123',
     }
 }
 """
@@ -94,7 +97,7 @@ DATABASES = {
     }
 }
 
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
