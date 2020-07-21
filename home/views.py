f4d308d2c324fbcde5a98cab1498775372703374
soumia_saadoun <es_saadoun@esi.dz>
2020-07-21 22:19:01 +0100

    new version

--- home/views.py
+++ home/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from Accidents.models import Sheet1
+from .models import Sheet1
 
 # Create your views here.
-def home(requests):
-    return render(requests,'home/welcome.html')
