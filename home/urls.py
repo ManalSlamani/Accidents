ea66c71be76501f746fc44e117692b896de74c18
soumia_saadoun <es_saadoun@esi.dz>
2020-07-29 03:10:39 +0100

    rien

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('', views.home, name='home'),
     path(r'^api/data/$', views.get_data, name='api-data')
-}
+]
